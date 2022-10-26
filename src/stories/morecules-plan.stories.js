0e882210cc74b3b70024980cd83f3921dbce22cd
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2022-10-26 19:51:12 +0900

    plan途中まで

---
+++ src/stories/morecules-plan.stories.js
@@ -0,0 +1,6 @@
+import Item_Plan from "../components/molecules/plan/plan.html";
+import "../components/molecules/plan/plan.scss";
+
+export default { title: "molecules/Plan" }
+
+export const Plan = () => Item_Plan;
