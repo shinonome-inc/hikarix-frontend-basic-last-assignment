bd92dbae57c5ba166a43a36d662deba3163a8a37
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2022-11-22 21:49:02 +0900

    一時保存

---
+++ src/stories/atoms-dropdown-menu..stories.js
@@ -0,0 +1,6 @@
+import Item_Dropdown_menu  from "../components/atoms/header/dropdown-menu.html";
+import "../components/atoms/header/dropdown-menu.scss";
+
+export default { title: "Atoms/Header/Dropdown_menu" }
+
+export const Dropdown_menu = () => Item_Dropdown_menu;
---
+++ src/stories/atoms-navlink.stories.js
@@ -0,0 +1,6 @@
+import Item_Nav_link  from "../components/atoms/header/nav-link.html";
+import "../components/atoms/header/nav-link.scss";
+
+export default { title: "Atoms/Header/Nav_link" }
+
+export const Nav_link = () => Item_Nav_link;
