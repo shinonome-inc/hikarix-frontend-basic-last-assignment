1c0bb805d95b1ff4c4ce92e7e5e2620565c57075
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2022-12-13 17:42:22 +0900

    header

---
+++ src/stories/morecules-nav-list.stories.js
@@ -0,0 +1,6 @@
+import Item_Nav_list from "../components/molecules/header/nav-list.html";
+import "../components/molecules/header/nav-list.scss";
+
+export default { title: "Molecules/nav-list" }
+
+export const Nav_list = () => Item_Nav_list;
