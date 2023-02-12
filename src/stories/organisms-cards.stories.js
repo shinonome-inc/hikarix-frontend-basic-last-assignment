3d163d305c7ede174b418900ab4c4ca48237d800
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-12 21:09:40 +0900

    Revert "WIP"
    This reverts commit 7f497cee153226d2015dd17057d39c41689dde60.

--- src/stories/organisms-cards.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Cards from "../components/organisms/cards/cards.html";
-import "../components/organisms/cards/cards.scss";
-
-export default { title: "Organisms/Cards" };
-
-export const Cards = () => Item_Cards;
