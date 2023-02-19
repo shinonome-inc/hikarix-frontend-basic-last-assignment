1788ed6902bd5fd03bc2192c8a804dba4f369d0f
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-19 21:33:39 +0900

    news-and-events

---
+++ src/stories/templates-news-and-events.stories.js
@@ -0,0 +1,6 @@
+import Item_News_And_Events from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
+
+export default { title: "Templates/NewsAndEvents" };
+
+export const NewsAndEvents = () => Item_News_And_Events;
